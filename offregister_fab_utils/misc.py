649259d7476000f3ffb2a7db2ea61a31140b6fa7
Samuel Marks <samuelmarks@gmail.com>
2017-01-15 16:45:44 +1100

    Implemented `merge_funcs`, e.g.: for inter-offregister deps on each func

--- offregister_fab_utils/misc.py
+++ offregister_fab_utils/misc.py
@@ -1,4 +1,7 @@
+from itertools import imap
+
 from fabric.api import local, sudo
+
 from offregister_fab_utils.fs import cmd_avail
 
 
@@ -8,3 +11,27 @@ def ubuntu_install_curl():
         local('echo {command} is already installed'.format(command=command))
     else:
         sudo('apt-get install -y curl')
+
+
+def process_funcs(*funcs):
+    def process(*args, **kwargs):
+        return dict(imap(lambda g: (g.__name__, g(*args, **kwargs)), funcs))
+
+    return process
+
+
+def merge_func(f):
+    def inner(*args, **kwargs):
+        return {'_merge': process_funcs(f)(*args, **kwargs)}
+
+    return inner
+
+
+def merge_funcs(*funcs):
+    def outer(f):
+        def inner(*args, **kwargs):
+            return {'_merge': process_funcs(f, *funcs)(*args, **kwargs)}
+
+        return inner
+
+    return outer
