ea4941c9017ff97ef57d9b80f599eb14021f358f
Samuel Marks <samuelmarks@gmail.com>
2018-04-17 14:38:43 +1000

    [yum] Added analogous to apt; [misc] upload_template_fmt, extended fabric's version with new '---{foo}ssss' syntax

--- offregister_fab_utils/__init__.py
+++ offregister_fab_utils/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 __author__ = 'Samuel Marks'
-__version__ = '0.0.11'
+__version__ = '0.0.12'
 
 skip_apt_update = False
+skip_yum_update = False
--- offregister_fab_utils/misc.py
+++ offregister_fab_utils/misc.py
@@ -1,14 +1,18 @@
+import operator
+
+from StringIO import StringIO
 from collections import namedtuple
+from functools import partial
 from itertools import imap
 from tempfile import mkdtemp
+from os import path, os
 
-from fabric.api import local, sudo
-from fabric.operations import get
+from fabric.api import run, sudo, hide, settings, env, put, abort, get
+from fabric.contrib.files import _expand_path, exists
+from fabric.utils import apply_lcwd
 from offutils import get_sorted_strnum
-from os import path
 
 from offregister_fab_utils.ubuntu.version import ubuntu_version
-import operator
 
 
 def process_funcs(*funcs):
@@ -88,3 +92,114 @@ def get_load_remote_file(directory, filename, use_sudo=True, load_f=lambda ident
     get(local_path=tmpdir, remote_path=remote_path, use_sudo=use_sudo)
     with open(path.join(tmpdir, filename)) as f:
         return namedtuple('_', ('remote_path', 'content'))(remote_path, load_f(f))
+
+
+def upload_template_fmt(filename, destination, context=None, use_jinja=False, use_fmt=False,
+                        template_dir=None, use_sudo=False, backup=True, mirror_local_mode=False,
+                        mode=None, pty=None, keep_trailing_newline=False, temp_dir=''):
+    """
+    Render and upload a template text file to a remote host.
+
+    Returns the result of the inner call to `~fabric.operations.put` -- see its
+    documentation for details.
+
+    ``filename`` should be the path to a text file, which may contain `Python
+    string interpolation formatting
+    <http://docs.python.org/library/stdtypes.html#string-formatting>`_ and will
+    be rendered with the given context dictionary ``context`` (if given.)
+
+    Alternately, if ``use_jinja`` is set to True and you have the Jinja2
+    templating library available, Jinja will be used to render the template
+    instead. Templates will be loaded from the invoking user's current working
+    directory by default, or from ``template_dir`` if given.
+
+    The resulting rendered file will be uploaded to the remote file path
+    ``destination``.  If the destination file already exists, it will be
+    renamed with a ``.bak`` extension unless ``backup=False`` is specified.
+
+    By default, the file will be copied to ``destination`` as the logged-in
+    user; specify ``use_sudo=True`` to use `sudo` instead.
+
+    The ``mirror_local_mode``, ``mode``, and ``temp_dir`` kwargs are passed
+    directly to an internal `~fabric.operations.put` call; please see its
+    documentation for details on these two options.
+
+    The ``pty`` kwarg will be passed verbatim to any internal
+    `~fabric.operations.run`/`~fabric.operations.sudo` calls, such as those
+    used for testing directory-ness, making backups, etc.
+
+    The ``keep_trailing_newline`` kwarg will be passed when creating
+    Jinja2 Environment which is False by default, same as Jinja2's
+    behaviour.
+
+    .. versionchanged:: 1.1
+        Added the ``backup``, ``mirror_local_mode`` and ``mode`` kwargs.
+    .. versionchanged:: 1.9
+        Added the ``pty`` kwarg.
+    .. versionchanged:: 1.11
+        Added the ``keep_trailing_newline`` kwarg.
+    .. versionchanged:: 1.11
+        Added the  ``temp_dir`` kwarg.
+    """
+    func = use_sudo and sudo or run
+    if pty is not None:
+        func = partial(func, pty=pty)
+    # Normalize destination to be an actual filename, due to using StringIO
+    with settings(hide('everything'), warn_only=True):
+        if func('test -d %s' % _expand_path(destination)).succeeded:
+            sep = "" if destination.endswith('/') else "/"
+            destination += sep + os.path.basename(filename)
+
+    # Use mode kwarg to implement mirror_local_mode, again due to using
+    # StringIO
+    if mirror_local_mode and mode is None:
+        mode = os.stat(apply_lcwd(filename, env)).st_mode
+        # To prevent put() from trying to do this
+        # logic itself
+        mirror_local_mode = False
+
+    # Process template
+    text = None
+    if use_jinja:
+        try:
+            template_dir = template_dir or os.getcwd()
+            template_dir = apply_lcwd(template_dir, env)
+            from jinja2 import Environment, FileSystemLoader
+            jenv = Environment(loader=FileSystemLoader(template_dir),
+                               keep_trailing_newline=keep_trailing_newline)
+            text = jenv.get_template(filename).render(**context or {})
+            # Force to a byte representation of Unicode, or str()ification
+            # within Paramiko's SFTP machinery may cause decode issues for
+            # truly non-ASCII characters.
+            text = text.encode('utf-8')
+        except ImportError:
+            import traceback
+            tb = traceback.format_exc()
+            abort(tb + "\nUnable to import Jinja2 -- see above.")
+    else:
+        if template_dir:
+            filename = os.path.join(template_dir, filename)
+        filename = apply_lcwd(filename, env)
+        with open(os.path.expanduser(filename)) as inputfile:
+            text = inputfile.read()
+        if context:
+            text = text.format(**context) if use_fmt else text % context
+
+        # Force to a byte representation of Unicode, or str()ification
+        # within Paramiko's SFTP machinery may cause decode issues for
+        # truly non-ASCII characters.
+        text = text.encode('utf-8')
+
+    # Back up original file
+    if backup and exists(destination):
+        func("cp %s{,.bak}" % _expand_path(destination))
+
+    # Upload the file.
+    return put(
+        local_path=StringIO(text),
+        remote_path=destination,
+        use_sudo=use_sudo,
+        mirror_local_mode=mirror_local_mode,
+        mode=mode,
+        temp_dir=temp_dir
+    )
