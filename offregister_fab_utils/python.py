a3837db5ef151dfc77160cb8aa37c78ba11e9016
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-25 23:36:51 +1000

    [python] First attempt at pip installer

---
+++ offregister_fab_utils/python.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+from platform import python_version_tuple
+
+from offregister_fab_utils.apt import Package
+
+if python_version_tuple()[0] == '2':
+    from itertools import imap, ifilter
+else:
+    imap = map
+    ifilter = filter
+
+from fabric.operations import _run_command
+
+
+def _cleanup_pkg_name(pkg):  # type: (str) -> str
+    pkg = pkg.partition('[')[0]
+    apache_s = 'apache-'
+    apache = pkg.startswith(apache_s)
+    if apache:
+        return pkg[len(apache_s):]
+    return pkg
+
+
+def is_not_installed(python=None, use_sudo=False, *packages):
+    if python is None:
+        python = _run_command('which python', sudo=use_sudo)
+
+    packages = (package
+                for packages_ in packages
+                for package in packages_)
+
+    return tuple(ifilter(None, imap(
+        lambda package: _run_command(
+            '{python} -c '
+            "'import pkgutil; exit(0 if pkgutil.find_loader({package}) else 2)'".format(
+                python=python, package="'{}'".format(_cleanup_pkg_name(package))
+            ),
+            sudo=use_sudo, warn_only=True, quiet=True).failed and package,
+        packages
+    )))
+
+
+def pip_depends(python=None, use_sudo=False, *packages):
+    if python is None:
+        python = _run_command('which python', sudo=use_sudo)
+    more_to_install = is_not_installed(python, use_sudo, *packages)
+    if not more_to_install:
+        return None
+    print('more_to_install:', more_to_install)
+    return _run_command('{python} -m pip install {packages}'.format(
+        python=python,
+        packages=' '.join(pkg.name if isinstance(pkg, Package)
+                          else pkg for pkg in more_to_install))
+    )
